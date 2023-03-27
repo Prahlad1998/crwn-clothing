8ba8174a0ffa641e1a040b9aed96dcba05052207
Prahlad1998 <76807359+Prahlad1998@users.noreply.github.com>
2023-03-27 20:33:29 +0530

    Created checkout page along with checkout cart functionality

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -13,14 +13,33 @@ const addCartItem=(cartItems,productToAdd)=>{
     return [...cartItems,{...productToAdd, quantity:1 }];
 
 };
+const removeCartItem=(cartItems,cartItemToRemove)=>{
+    //find the cart item to remove which is in the cart already
+    const existingCartItems= cartItems.find(
+        (cartItem)=>cartItem.id===cartItemToRemove.id);
+    //check if the quantity is equal to one,if it is then remove the item from the cart
+    if (existingCartItems.quantity===1){
+        return cartItems.filter(cartItem=>cartItem.id!=  cartItemToRemove.id);
+    }
+
+    //return back cart items with reduced quantity
+    return cartItems.map((cartItem)=>cartItem.id === cartItemToRemove.id?{...cartItem,quantity:cartItem.quantity-1}:cartItem
+     );
+}
+const clearCartItem=(cartItems,cartItemToClear)=>
+    cartItems.filter((cartItem)=>cartItem.id != cartItemToClear.id);
 
 export const CartContext=createContext({
     isCartOpen:false,
     setIsCartOpen:()=>{
     },
     cartItems:[],
-    addItemToCart:()=>{},//function that will be used to add item to the cart
+    addItemToCart:()=>{},
+    //function that will be used to add item to the cart
+    removeItemFromCart:()=>{},
     cartCount:0,
+    clearItemFromCart:()=>{},
+   cartTotal:0,
 })
 
 export const CartProvider=({children})=>{
@@ -28,18 +47,39 @@ export const CartProvider=({children})=>{
     const [isCartOpen,setIsCartOpen]=useState(false);
     const [cartItems,setCartItems]=useState([]);
     const [cartCount,setCartCount]=useState(0);
+    const [cartTotal,setCartTotal]=useState(0);
     // we will use useeffect hooks to count the total item in the cart. we will pass a callback function,which will run whenever the dependencies i.e the cartitem's quantity value changes.
     useEffect(()=>{
         const newCartCount=cartItems.reduce((total,cartItem)=>total+cartItem.quantity,0)
         setCartCount(newCartCount);
+    },[cartItems]);
+//the follwing useEffect for calculating total cart value
+    useEffect(()=>{
+        const newCartTotal=cartItems.reduce((total,cartItem)=>total+cartItem.quantity*cartItem.price,0)
+        setCartTotal(newCartTotal);
     },[cartItems]);
 
 
     //the following function will be triggered whenever the user will cick the add to cart button
     const addItemToCart=(productToAdd)=>{
         setCartItems(addCartItem(cartItems,productToAdd));
-
     }
-    const value ={isCartOpen,setIsCartOpen,addItemToCart,cartItems,cartCount};
+    const removeItemFromCart=(cartItemToRemove)=>{
+        setCartItems(removeCartItem(cartItems,cartItemToRemove));
+    }
+    const clearItemFromCart=(cartItemToClear)=>{
+        setCartItems(clearCartItem(cartItems,cartItemToClear));
+    }
+    const value =
+    {
+        isCartOpen,
+        setIsCartOpen,
+        addItemToCart,
+        removeItemFromCart,
+        clearItemFromCart,
+        cartItems,
+        cartCount,
+        cartTotal,
+    };
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 }
